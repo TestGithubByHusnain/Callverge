bedb9d5e56527ca9fd37f420e49303c3c2aa11a0
Husnain Ali <husnain.dev.009@gmail.com>
2025-11-13 12:25:00 +0500

    Initial setup

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
