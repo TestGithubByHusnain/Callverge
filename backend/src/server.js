eb9fe26be83fefa3837e11f6967c9da854cf7fb8
Husnain Ali <husnain.dev.009@gmail.com>
2025-11-15 12:14:10 +0500

    Database and tools setup.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
@@ -18,4 +19,15 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
